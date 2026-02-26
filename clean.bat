7e20ef7cc1dad0b77b5de7b8f2e0624e66147382
ThomasRicart <ricart@laposte.net>
2026-02-26 17:18:07 +0100

    c

---
+++ clean.bat
@@ -0,0 +1,13 @@
+del /S /Q *.aux *.pytxcode *.thm *.log *.out *.toc *.lof *.lot *.nav *.snm *.synctex.gz *.bbl *.blg *.fls *.aux *.fdb_latexmk
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: Recherche récursive des dossiers "pythontex-files*"
+for /d /r %%D in (pythontex-files*) do (
+    echo Suppression du dossier : "%%D"
+    rd /s /q "%%D"
+)
+
+echo Suppression terminée.
+pause
